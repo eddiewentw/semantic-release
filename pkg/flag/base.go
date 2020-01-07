d79bce11ce91454d8f9dbc0ec2ed6f98ef8cbbc0
Eddie Wen <eddiewen.tw@gmail.com>
2020-01-07 22:21:13 +0800

    chore: extends debug log function

--- pkg/flag/base.go
+++ pkg/flag/base.go
@@ -2,8 +2,10 @@ package flag
 
 const DRY_RUN_FLAG = "--dry-run"
 const FIRST_RELEASE_FLAG = "--first-release"
+const DEBUG_FLAG = "--debug"
 
 type Args struct {
 	IsDryRun       bool
 	IsFirstRelease bool
+	IsDebug        bool
 }
